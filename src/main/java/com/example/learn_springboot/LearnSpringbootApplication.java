5ec0460508b711948e7b8b4db5809f47353fa87b
Farhan Naim <nahraf.mian06@gmail.com>
2026-08-07 21:58:56 -0600

    add student class

--- src/main/java/com/example/learn_springboot/LearnSpringbootApplication.java
+++ src/main/java/com/example/learn_springboot/LearnSpringbootApplication.java
@@ -1,13 +1,33 @@
 package com.example.learn_springboot;
 
+import com.example.learn_springboot.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class LearnSpringbootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearnSpringbootApplication.class, args);
 	}
 
+	@GetMapping
+	public List<Student> hello() {
+		return List.of(
+				new Student(
+						1L,
+						"Mariam",
+						"mariam@gmail.com",
+						LocalDate.of(2000, Month.JANUARY, 5),
+						21
+				)
+		);
+	}
 }
