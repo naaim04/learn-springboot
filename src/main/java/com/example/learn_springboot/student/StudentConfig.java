a5a889f17e9f13c9cee26df1be31c40f6c0559ad
Farhan Naim <nahraf.mian06@gmail.com>
2026-08-11 14:28:06 -0600

    add student data persistence

---
+++ src/main/java/com/example/learn_springboot/student/StudentConfig.java
@@ -0,0 +1,38 @@
+package com.example.learn_springboot.student;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student mariam = new Student(
+                    "Mariam",
+                    "mariam@gmail.com",
+                    LocalDate.of(2000, Month.JANUARY, 5),
+                    21
+            );
+
+            Student alex = new Student(
+                    "Alex",
+                    "alex@gmail.com",
+                    LocalDate.of(2004, Month.JANUARY, 5),
+                    21
+            );
+
+            repository.saveAll(
+                    List.of(mariam, alex)
+            );
+
+        };
+    }
+}
