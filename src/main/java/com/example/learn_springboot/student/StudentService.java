2c93dad5b849ab20ee6077b2591289441bf1f973
Farhan Naim <nahraf.mian06@gmail.com>
2026-08-11 14:14:28 -0600

    create repository

--- src/main/java/com/example/learn_springboot/student/StudentService.java
+++ src/main/java/com/example/learn_springboot/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.learn_springboot.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -9,16 +10,16 @@ import java.util.List;
 
 @Service
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return List.of(
-                new Student(
-                        1L,
-                        "Mariam",
-                        "mariam@gmail.com",
-                        LocalDate.of(2000, Month.JANUARY, 5),
-                        21
-                )
-        );
+        return studentRepository.findAll();
     }
 }
 
